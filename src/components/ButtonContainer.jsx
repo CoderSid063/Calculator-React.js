c7c1933cb7834b14ac8a4c5bfe07b0611e64fe76
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-14 10:10:08 +0530

    Creating Multiple Button

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,12 +1,10 @@
 import styles from "./ButtonContainer.module.css";
 
 const ButtonContainer = () => {
+  const buttonName = ["c","1","2","+","3","4","-","5","6","*","7","8","/","9","0","="]
   return (
     <div className={styles.buttonContainer}>
-      <button className={styles.button}>c</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
+      {buttonName.map(buttons => <button className={styles.button}>{buttons}</button> )}
     </div>
   );
 };
