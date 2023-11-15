65cebb69073e288bf31c207c29e2d959270d1361
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-15 15:43:02 +0530

    Passing function and State Manageing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import Display from "./components/Display";
 import ButtonContainer from "./components/ButtonContainer";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className={styles.calculator}>
-    <Display></Display>
-    <ButtonContainer></ButtonContainer>
-  </div>
-   
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "c") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayVal = calVal + buttonText;
+      setCalVal(newDisplayVal);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonContainer onButtonClick={onButtonClick}></ButtonContainer>
+    </div>
+  );
 }
 
 export default App;
